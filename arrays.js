2d9f2df88de9d213f962666693c8aa1222a568b0
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-05-18 19:04:41 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,30 @@
+var chocolateBars = [
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles"]
+  
+function addElementToBeginningOfArray(array, n) {
+var myArray = [n, ...array]
+return myArray
+}
+
+function destructivelyAddElementToBeginningOfArray(array, n) {
+  array.unshift(n)
+  return array
+}
+
+function addElementToEndOfArray(array, n) {
+var myArray = [...array, n]
+return myArray
+}
+
+function destructivelyAddElementToEndOfArray(array, n) {
+  array.push(n)
+  return array
+}
+
+function accessElementInArray(array, index) {
+return array[index]
+}
+
